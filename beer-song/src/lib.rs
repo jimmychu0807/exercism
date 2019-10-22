67e44b6921e2c70aad1c91e1ee129947afcfbe73
Jimmy Chu <jimmychu0807@gmail.com>
2019-10-22 19:43:49 +0800

    one version of beer-song

--- beer-song/src/lib.rs
+++ beer-song/src/lib.rs
@@ -1,7 +1,35 @@
+extern crate strfmt;
+use strfmt::strfmt;
+use std::collections::HashMap;
+
 pub fn verse(n: u32) -> String {
-    unimplemented!("emit verse {}", n)
+  let verse_zero = "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n";
+
+  let verse_generic = "{first} of beer on the wall, {first} of beer.\nTake {pronoun} down and pass it around, {second} of beer on the wall.\n";
+
+  let mut verse_vals = HashMap::new();
+
+  if n == 0 {
+    strfmt(verse_zero, &verse_vals).unwrap()
+  } else if n == 1 {
+    verse_vals.insert("first".to_string(), n.to_string() + " bottle");
+    verse_vals.insert("second".to_string(), "no more bottles".to_string());
+    verse_vals.insert("pronoun".to_string(), "it".to_string());
+    strfmt(verse_generic, &verse_vals).unwrap()
+  } else {
+    let second_bottle = if n - 1 == 1 {"bottle"} else {"bottles"};
+    verse_vals.insert("first".to_string(), n.to_string() + " bottles");
+    verse_vals.insert("second".to_string(),
+      (n - 1).to_string() + " " + second_bottle);
+    verse_vals.insert("pronoun".to_string(), "one".to_string());
+    strfmt(verse_generic, &verse_vals).unwrap()
+  }
 }
 
 pub fn sing(start: u32, end: u32) -> String {
-    unimplemented!("sing verses {} to {}, inclusive", start, end)
+  (end..=start)
+    .rev()
+    .map(|n| verse(n))
+    .collect::<Vec<String>>()
+    .join("\n")
 }
