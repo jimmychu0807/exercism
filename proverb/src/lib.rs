c241928eb4cdd0aa0c6cc7f6665c6efb30d9b8ff
Jimmy Chu <jimmychu0807@gmail.com>
2019-10-23 06:57:28 +0800

    completed proverb

--- proverb/src/lib.rs
+++ proverb/src/lib.rs
@@ -1,3 +1,19 @@
 pub fn build_proverb(list: &[&str]) -> String {
-    unimplemented!("build a proverb from this list of items: {:?}", list)
+  if list.len() == 0 { return String::new(); }
+
+  let mut word_pair: Vec<(&str, &str)> = Vec::new();
+  list.iter().enumerate().for_each(|(i, word)| {
+    if i == list.len() - 1 { return; }
+    word_pair.push((word, list[i + 1]));
+  });
+
+  let mut proverb = word_pair
+    .iter()
+    .map(|(first, second)| {
+      format!("For want of a {} the {} was lost.\n", first, second)
+    })
+    .collect::<Vec<String>>()
+    .join("");
+  proverb.push_str(&format!("And all for the want of a {}.", list[0]));
+  proverb
 }
---
+++ proverb/src/main.rs
@@ -0,0 +1,5 @@
+use proverb;
+
+fn main() {
+  println!("{}", proverb::build_proverb(&["nail", "shoe"]));
+}
