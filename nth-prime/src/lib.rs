463298dce5e46130aec60c15295e81b850f7b86c
Jimmy Chu <jimmychu0807@gmail.com>
2019-10-22 16:51:09 +0800

    solved nth-prime, and work on beer-song

--- nth-prime/src/lib.rs
+++ nth-prime/src/lib.rs
@@ -1,16 +1,24 @@
-fn is_prime(n: &u32) -> bool {
+fn is_prime(n: u32) -> bool {
+  let upto = (n as f32).sqrt().ceil() as u32;
+  let mut trial = 2;
+
+  // It can only be 0, 1, 2
+  if trial > upto { return true; }
+  while trial <= upto {
+    if n % trial == 0 { return false; }
+    trial += if trial % 2 == 0 { 1 } else { 2 };
+  }
   true
 }
 
 pub fn nth(n: u32) -> u32 {
-  let mut prime_arr = vec![2, 3, 5, 7, 11, 13];
+  let mut prime_arr = vec![2];
   let mut trial = prime_arr.last().unwrap().clone();
 
-  while n < (prime_arr.len() as u32) {
+  while n >= (prime_arr.len() as u32) {
     trial += 1;
-    if !is_prime(&trial) { continue; }
+    if !is_prime(trial) { continue; }
     prime_arr.push(trial);
   }
-
   return *prime_arr.last().unwrap();
 }
