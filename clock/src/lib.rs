614c8a80f6c61261f6c3b5e9c3b6201690202c0a
Jimmy Chu <jimmychu0807@gmail.com>
2019-10-23 12:37:36 +0800

    finish clock

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -1,15 +1,41 @@
-pub struct Clock;
+use std::fmt;
+
+#[derive(Debug, PartialEq)]
+pub struct Clock {
+  hours: u32,
+  minutes: u32
+}
 
 impl Clock {
-    pub fn new(hours: i32, minutes: i32) -> Self {
-        unimplemented!(
-            "Construct a new Clock from {} hours and {} minutes",
-            hours,
-            minutes
-        );
+  pub fn new(hours: i32, minutes: i32) -> Self {
+    let (clock_hr, clock_min) = Self::add_time((0, 0), (hours, minutes));
+    Clock { hours: clock_hr, minutes: clock_min }
+  }
+
+  pub fn add_minutes(&self, minutes: i32) -> Self {
+    Clock::new(self.hours as i32, self.minutes as i32 + minutes)
+  }
+
+  // -- helper methods --
+  fn add_time(base: (u32, u32), offset: (i32, i32)) -> (u32, u32) {
+    let mut clock_min = base.1 as i32 + offset.1;
+    let mut hr_decrement = 0;
+    while clock_min < 0 {
+      clock_min += 60;
+      hr_decrement += 1;
     }
 
-    pub fn add_minutes(&self, minutes: i32) -> Self {
-        unimplemented!("Add {} minutes to existing Clock time", minutes);
-    }
+    let mut clock_hr = base.0 as i32 - hr_decrement + offset.0;
+    while clock_hr < 0 { clock_hr += 24; }
+
+    clock_hr = (clock_hr + clock_min / 60) % 24;
+    clock_min = clock_min % 60;
+    (clock_hr as u32, clock_min as u32)
+  }
+}
+
+impl fmt::Display for Clock {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    write!(f, "{:02}:{:02}", self.hours, self.minutes)
+  }
 }
---
+++ clock/src/main.rs
@@ -0,0 +1,12 @@
+use clock::Clock;
+
+fn main() {
+  println!("{}", Clock::new(0, 0));
+  println!("{}", Clock::new(0, 1));
+  println!("{}", Clock::new(0, -1));
+  println!("{}", Clock::new(0, 60));
+  println!("{}", Clock::new(0, 121));
+  println!("{}", Clock::new(-1, 0));
+  println!("{}", Clock::new(24, 0));
+  println!("{}", Clock::new(49, 0));
+}
