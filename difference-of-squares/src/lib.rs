975445ce4704f5be26df5a932523f1cdb7265639
Jimmy Chu <jimmychu0807@gmail.com>
2019-10-23 10:39:19 +0800

    completed difference of squares

--- difference-of-squares/src/lib.rs
+++ difference-of-squares/src/lib.rs
@@ -1,14 +1,15 @@
 pub fn square_of_sum(n: u32) -> u32 {
-    unimplemented!("square of sum of 1...{}", n)
+  let sum = (1..=n)
+    .fold(0, |mem, x| mem + x);
+  sum * sum
 }
 
 pub fn sum_of_squares(n: u32) -> u32 {
-    unimplemented!("sum of squares of 1...{}", n)
+  (1..=n)
+    .map(|x| x*x)
+    .fold(0, |mem, x| mem + x)
 }
 
 pub fn difference(n: u32) -> u32 {
-    unimplemented!(
-        "difference between square of sum of 1...{n} and sum of squares of 1...{n}",
-        n = n,
-    )
+  square_of_sum(n) - sum_of_squares(n)
 }
---
+++ difference-of-squares/src/main.rs
@@ -0,0 +1,7 @@
+use difference_of_squares as dos;
+
+fn main() {
+  println!("{}", dos::sum_of_squares(3));
+  println!("{}", dos::square_of_sum(3));
+  println!("{}", dos::difference(3));
+}
