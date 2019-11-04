bfde6963b8638549c7e300c72d3ef9a3da23aac5
Jimmy Chu <jimmychu0807@gmail.com>
2019-11-04 20:15:40 +0100

    completed a simple exercise

--- grains/src/lib.rs
+++ grains/src/lib.rs
@@ -1,7 +1,12 @@
 pub fn square(s: u32) -> u64 {
-    unimplemented!("grains of rice on square {}", s);
+  match s {
+    1..=64 => (2 as u64).pow(s - 1),
+    _ => panic!("Square must be between 1 and 64"),
+  }
 }
 
 pub fn total() -> u64 {
-    unimplemented!();
+  (1..=64)
+    .map(|n| square(n))
+    .sum()
 }
