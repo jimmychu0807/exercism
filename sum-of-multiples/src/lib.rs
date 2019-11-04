3dc5dac738b483974c113ae00dd500f79bc134a4
Jimmy Chu <jimmychu0807@gmail.com>
2019-11-04 19:50:09 +0100

    using destructure on the variable in filter closure

--- sum-of-multiples/src/lib.rs
+++ sum-of-multiples/src/lib.rs
@@ -1,5 +1,5 @@
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
   (1..limit)
-    .filter(|num| factors.iter().filter(|f| **f > 0).any(|f| num % f == 0))
+    .filter(|num| factors.iter().filter(|&&f| f > 0).any(|f| num % f == 0))
     .fold(0, |mem, x| mem + x)
 }
