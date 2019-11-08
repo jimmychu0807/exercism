90d4f9c9e6b5f6130467a32ed0727604c00a2cc6
Jimmy Chu <jimmychu0807@gmail.com>
2019-11-08 21:39:29 +0100

    completed

--- anagram/src/lib.rs
+++ anagram/src/lib.rs
@@ -1,9 +1,33 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
-    unimplemented!(
-        "For the '{}' word find anagrams among the following words: {:?}",
-        word,
-        possible_anagrams
-    );
+pub fn anagrams_for<'a>(word: &'a str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
+  let mut result = HashSet::<&'a str>::new();
+  let lower_word = word.to_lowercase();
+
+  for test in possible_anagrams {
+    let lower_test = test.to_lowercase();
+    if lower_word == lower_test || !is_anagram(&lower_word, &lower_test) { continue }
+    result.insert(*test);
+  }
+  result
+}
+
+fn is_anagram(word1: &str, word2: &str) -> bool {
+  let mut chars_cnt = HashMap::<char, u32>::new();
+  word1.chars().for_each(|c| {
+    match chars_cnt.contains_key(&c) {
+      true => chars_cnt.insert(c, chars_cnt.get(&c).unwrap() + 1),
+      false => chars_cnt.insert(c, 1),
+    };
+  });
+
+  for cw2 in word2.chars() {
+    match chars_cnt.get(&cw2) {
+      None => return false,
+      Some(cw2_cnt) if *cw2_cnt <= 0 => return false,
+      Some(cw2_cnt) => chars_cnt.insert(cw2, cw2_cnt - 1),
+    };
+  }
+
+  chars_cnt.iter().filter(|(_, v)| **v > 0).count() == 0
 }
