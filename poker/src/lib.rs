3adc8bcd51fcafd174ab7e2d69c583d34110ff4c
Jimmy Chu <jimmychu0807@gmail.com>
2019-11-25 12:50:11 +0800

    added Card object type

--- poker/src/lib.rs
+++ poker/src/lib.rs
@@ -11,6 +11,7 @@ use std::fmt;
 // K - 13
 // A - 1, 14
 
+#[derive(Debug)]
 enum PokerHandPattern {
   StraightFlush(u32),
   FourOfAKind(u32, u32),
@@ -35,20 +36,82 @@ impl PartialEq for PokerHandPattern {
   }
 }
 
+#[derive(Eq, Debug)]
+struct Card {
+  rank: u32,
+  suit: String,
+}
+
+impl Card {
+  fn new<'a>(card_str: &'a str) -> Card {
+    let card_str = card_str.to_uppercase();
+    let suit = card_str.chars().last().unwrap().to_string();
+    let rank_str = card_str.chars().take(card_str.chars().count() - 1).collect::<String>();
+    let rank = rank_str.parse::<u32>().or_else(|_| {
+      match rank_str {
+        rank_str if rank_str == "J" => Ok(11),
+        rank_str if rank_str == "Q" => Ok(12),
+        rank_str if rank_str == "K" => Ok(13),
+        rank_str if rank_str == "A" => Ok(14),
+        _ => Err(())
+      }
+    }).unwrap();
+    Card { rank, suit }
+  }
+}
+
+impl Ord for Card {
+  fn cmp(&self, other: &Self) -> Ordering {
+    if self.rank != other.rank { return self.rank.cmp(&other.rank) }
+
+    // closure
+    let suit_rank = |suit_str| match suit_str {
+      s if s == "S" => 4,
+      s if s == "H" => 3,
+      s if s == "C" => 2,
+      _ => 1
+    };
+    let self_suit_rank = suit_rank(&self.suit);
+    let other_suit_rank = suit_rank(&other.suit);
+    self_suit_rank.cmp(&other_suit_rank)
+  }
+}
+
+impl PartialOrd for Card {
+  fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+    Some(self.cmp(other))
+  }
+}
+
+impl PartialEq for Card {
+  fn eq(&self, other: &Self) -> bool {
+    self.rank == other.rank && self.suit == other.suit
+  }
+}
+
+#[derive(Debug)]
 struct PokerHand<'a> {
   hand_str: &'a str,
+  cards: Vec<Card>,
   pattern: PokerHandPattern,
 }
 
 impl<'a> PokerHand<'a> {
-  fn new(hand: &'a str) -> PokerHand {
-    PokerHand {
-      hand_str: hand,
-      pattern: Self::get_pattern(hand),
-    }
+  fn new(hand_str: &'a str) -> PokerHand {
+    let cards = Self::get_sorted_cards(hand_str);
+    PokerHand { hand_str, cards, pattern: Self::get_pattern(hand_str) }
+  }
+
+  fn get_sorted_cards(hand_str: &'a str) -> Vec<Card> {
+    let mut cards = hand_str.split(' ')
+      .map(|card_str| Card::new(card_str))
+      .collect::<Vec<_>>();
+    cards.sort();
+    cards
   }
 
   fn get_pattern(hand: &'a str) -> PokerHandPattern {
+    // TODO: fill in actual implementation
     PokerHandPattern::StraightFlush(14)
   }
 }
@@ -59,6 +122,9 @@ pub fn winning_hands<'a>(hands: &[&'a str]) -> Option<Vec<&'a str>> {
   let mut hands = hands.iter().map(|hand| PokerHand::new(*hand)).collect::<Vec<_>>();
   hands.sort_by(|a, b| b.pattern.partial_cmp(&a.pattern).unwrap());
   let largest = hands.first().unwrap();
+
+  println!("hands: {:?}", hands);
+  println!("largest: {:?}", largest);
 
   // could be multiple answers, so we want to retrieve them all
   let res = hands
