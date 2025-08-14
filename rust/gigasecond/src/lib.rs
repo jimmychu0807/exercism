0ff16483648f9a3342fa06230f044165da69fc37
Jimmy Chu <898091+jimmychu0807@users.noreply.github.com>
2025-08-14 23:24:35 +0800

    updated with solution #3 & #4 (#2)
    * updated
    
    * updated

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -1,6 +1,8 @@
-use time::PrimitiveDateTime as DateTime;
+use time::{Duration, PrimitiveDateTime as DateTime};
+
+const GIGA_SECONDS: i64 = 1_000_000_000;
 
 // Returns a DateTime one billion seconds after start.
 pub fn after(start: DateTime) -> DateTime {
-    todo!("What time is a gigasecond later than {start}");
+	start.checked_add(Duration::seconds(GIGA_SECONDS)).unwrap()
 }
--- rust/reverse-string/tests/reverse-string.rs
+++ rust/reverse-string/tests/reverse-string.rs
@@ -9,48 +9,48 @@ use reverse_string::*;
 
 /// Process a single test case for the property `reverse`
 fn process_reverse_case(input: &str, expected: &str) {
-    assert_eq!(&reverse(input), expected)
+	assert_eq!(&reverse(input), expected)
 }
 
 #[test]
 /// empty string
 fn test_an_empty_string() {
-    process_reverse_case("", "");
+	process_reverse_case("", "");
 }
 
 #[test]
 /// a word
 fn test_a_word() {
-    process_reverse_case("robot", "tobor");
+	process_reverse_case("robot", "tobor");
 }
 
 #[test]
 /// a capitalized word
 fn test_a_capitalized_word() {
-    process_reverse_case("Ramen", "nemaR");
+	process_reverse_case("Ramen", "nemaR");
 }
 
 #[test]
 /// a sentence with punctuation
 fn test_a_sentence_with_punctuation() {
-    process_reverse_case("I'm hungry!", "!yrgnuh m'I");
+	process_reverse_case("I'm hungry!", "!yrgnuh m'I");
 }
 
 #[test]
 /// a palindrome
 fn test_a_palindrome() {
-    process_reverse_case("racecar", "racecar");
+	process_reverse_case("racecar", "racecar");
 }
 
 #[test]
 /// wide characters
 fn test_wide_characters() {
-    process_reverse_case("子猫", "猫子");
+	process_reverse_case("子猫", "猫子");
 }
 
 #[test]
 #[cfg(feature = "grapheme")]
 /// grapheme clusters
 fn test_grapheme_clusters() {
-    process_reverse_case("uüu", "uüu");
+	process_reverse_case("uüu", "uüu");
 }
