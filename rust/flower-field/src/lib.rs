949e8bfe9e76338612a337db9d9c50754773bcf8
Jimmy Chu <898091+jimmychu0807@users.noreply.github.com>
2025-08-24 22:34:37 +0800

    feat: completed problem-08: flower-field (#6)

--- rust/flower-field/src/lib.rs
+++ rust/flower-field/src/lib.rs
@@ -1,5 +1,54 @@
 pub fn annotate(garden: &[&str]) -> Vec<String> {
-	todo!(
-		"\nAnnotate each square of the given garden with the number of flowers that surround said square (blank if there are no surrounding flowers):\n{garden:#?}\n"
-	);
+	if garden.is_empty() {
+		return vec![];
+	}
+
+	let mut result: Vec<String> = vec![];
+
+	let num_row = garden.len();
+	let num_col = garden[0].len();
+
+	for row_idx in 0..num_row {
+		let mut result_row = String::from("");
+
+		for col_idx in 0..num_col {
+			// we are looping thru each cell
+			let mut num_flower = 0_u32;
+
+			// if it is a `*` just push that and check the next square
+			if garden[row_idx].as_bytes()[col_idx] == 42 {
+				result_row.push('*');
+				continue;
+			}
+
+			// looping over the neighboring eight cells
+			for di in [-1_i32, 0, 1] {
+				for dj in [-1_i32, 0, 1] {
+					if di == 0 && dj == 0 {
+						continue;
+					}
+
+					let neighbor_row = dj + row_idx as i32;
+					let neighbor_col = di + col_idx as i32;
+
+					if neighbor_row < 0 || neighbor_row >= num_row as i32 {
+						continue;
+					}
+					if neighbor_col < 0 || neighbor_col >= num_col as i32 {
+						continue;
+					}
+					if garden[neighbor_row as usize].as_bytes()[neighbor_col as usize] == 42 {
+						num_flower += 1;
+					}
+				}
+			}
+
+			let target_char =
+				if num_flower == 0 { ' ' } else { char::from_digit(num_flower, 10).unwrap() };
+			result_row.push(target_char);
+		}
+		result.push(result_row);
+	}
+
+	result
 }
