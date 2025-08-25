4a54187bc87d35499f80863e981f0079cf165290
Jimmy Chu <898091+jimmychu0807@users.noreply.github.com>
2025-08-25 11:19:47 +0800

    feat: completed problem-09, problem-10 (#7)
    * feat: completed problem-09 luhn
    
    * updated
    
    * feat: completed problem-10 armstrong-number

--- rust/README.md
+++ rust/README.md
@@ -10,3 +10,5 @@ https://exercism.org/tracks/rust
 | 6   | Space Age      | [`space-age`](./space-age)            |  ✅ |
 | 7   | Sublist        | [`sublist`](./sublist)                |  ✅ |
 | 8   | Flower Field   | [`flower-field`](./flower-field)      |  ✅ |
+| 9   | Luhn           | [`luhn`](./luhn)                      |  ✅ |
+| 10  | Armstrong Numbers | [`armstrong-number`](./armstrong-number) | ✅ |
--- rust/armstrong-numbers/src/lib.rs
+++ rust/armstrong-numbers/src/lib.rs
@@ -1,9 +1,14 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-  let num_str = num.to_string();
-  let strlen = num_str.len() as u32;
-  let summed = num_str
-    .chars()
-    .map(|char| char.to_digit(10).unwrap().pow(strlen))
-    .fold(0, |mem, v| mem + v);
-  summed == num
+	let num_str = num.to_string();
+	let strlen = num_str.len() as u32;
+
+	let check_summed = num_str
+		.chars()
+		.map(|c| String::from(c).parse::<u32>().unwrap().pow(strlen))
+		.try_fold(0_u32, |acc, val| acc.checked_add(val));
+
+	match check_summed {
+		Some(val) => val == num,
+		None => false,
+	}
 }
