84d3e9ee621d7b275308774d3b444d44f3bee09e
Jimmy Chu <898091+jimmychu0807@users.noreply.github.com>
2025-08-27 14:10:25 +0800

    feat: completed problem-23

--- rust/matching-brackets/src/lib.rs
+++ rust/matching-brackets/src/lib.rs
@@ -1,25 +1,22 @@
 pub fn brackets_are_balanced(string: &str) -> bool {
-  let mut stack: Vec<char> = Vec::new();
-  for b in string.chars() {
-    if b == '(' || b == '[' || b == '{' {
-      stack.push(b);
-      continue;
-    }
+	let mut stack: Vec<char> = Vec::new();
 
-    if b == ')' || b == ']' || b == '}' {
-      let last = match stack.pop() {
-        Some(val) => val,
-        None => return false,
-      };
-      let match_bracket = match last {
-        last if last == '(' => b == ')',
-        last if last == '[' => b == ']',
-        last if last == '{' => b == '}',
-        _ => return false,
-      };
-
-      if !match_bracket { return false; }
-    }
-  }
-  stack.is_empty()
+	for c in string.chars() {
+		match c {
+			'(' | '[' | '{' => stack.push(c),
+			')' | ']' | '}' => {
+				let open = match c {
+					')' => '(',
+					']' => '[',
+					'}' => '{',
+					_ => panic!("should not reach here"),
+				};
+				if stack.pop() != Some(open) {
+					return false;
+				}
+			}
+			_ => continue,
+		}
+	}
+	stack.is_empty()
 }
--- rust/matching-brackets/src/main.rs
+++ rust/matching-brackets/src/main.rs
@@ -1,11 +1,11 @@
 use matching_brackets as mb;
 
 fn main() {
-  println!("{}", mb::brackets_are_balanced("]"));
-  println!("{}", mb::brackets_are_balanced("["));
-  println!("{}", mb::brackets_are_balanced("[)"));
-  println!("{}", mb::brackets_are_balanced("[]"));
-  println!("{}", mb::brackets_are_balanced("{[]}"));
-  println!("{}", mb::brackets_are_balanced("{([])}"));
-  println!("{}", mb::brackets_are_balanced("{[}]"));
+	println!("{}", mb::brackets_are_balanced("]"));
+	println!("{}", mb::brackets_are_balanced("["));
+	println!("{}", mb::brackets_are_balanced("[)"));
+	println!("{}", mb::brackets_are_balanced("[]"));
+	println!("{}", mb::brackets_are_balanced("{[]}"));
+	println!("{}", mb::brackets_are_balanced("{([])}"));
+	println!("{}", mb::brackets_are_balanced("{[}]"));
 }
