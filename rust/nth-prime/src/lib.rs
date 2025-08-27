ab9876c18e2cd142665a9d69c36526570cbcfeb2
Jimmy Chu <898091+jimmychu0807@users.noreply.github.com>
2025-08-27 08:11:11 +0800

    feat: completed problem-16

--- rust/nth-prime/src/lib.rs
+++ rust/nth-prime/src/lib.rs
@@ -1,24 +1,27 @@
 fn is_prime(n: u32) -> bool {
-  let upto = (n as f32).sqrt().ceil() as u32;
-  let mut trial = 2;
-
-  // It can only be 0, 1, 2
-  if trial > upto { return true; }
-  while trial <= upto {
-    if n % trial == 0 { return false; }
-    trial += if trial % 2 == 0 { 1 } else { 2 };
-  }
-  true
+	if n < 2 {
+		return false;
+	}
+	!(2..=n.isqrt()).any(|i| n % i == 0)
 }
 
 pub fn nth(n: u32) -> u32 {
-  let mut prime_arr = vec![2];
-  let mut trial = prime_arr.last().unwrap().clone();
+	let next_prime = |i: u32| -> u32 {
+		let mut t = i;
+		loop {
+			if is_prime(t) {
+				break;
+			}
+			t += 1;
+		}
+		t
+	};
 
-  while n >= (prime_arr.len() as u32) {
-    trial += 1;
-    if !is_prime(trial) { continue; }
-    prime_arr.push(trial);
-  }
-  return *prime_arr.last().unwrap();
+	let mut primes = vec![2];
+
+	while n != (primes.len() as u32) - 1 {
+		primes.push(next_prime(primes.last().unwrap() + 1));
+	}
+
+	*primes.last().unwrap()
 }
