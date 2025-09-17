9ed6fdded77f676b7e2863448c2673d810b9bc8c
Jimmy Chu <898091+jimmychu0807@users.noreply.github.com>
2025-09-17 14:09:43 +0800

    added problem-34: dot-dsl

--- rust/dot-dsl/src/lib.rs
+++ rust/dot-dsl/src/lib.rs
@@ -1,1 +1,36 @@
-lib_macro.rs
+pub mod graph_items;
+
+pub mod graph {
+	pub use crate::graph_items;
+	pub use graph_items::{edge::Edge, node::Node};
+
+	#[derive(Debug, Default, Clone)]
+	pub struct Attr {}
+
+	#[derive(Debug, Default, Clone)]
+	pub struct Graph {
+		pub edges: Vec<Edge>,
+		pub nodes: Vec<Node>,
+		pub attrs: Vec<Attr>,
+	}
+
+	impl Graph {
+		pub fn new() -> Self {
+			Self::default()
+		}
+
+		// pub fn with_nodes(mut self, nodes: &[Node]) -> Self {
+		// 	self.nodes = nodes.into();
+		// 	self
+		// }
+
+		// pub fn with_edges(mut self, edges: &Vec<Edge>) -> Self {
+		// 	self.edges = (*edges).clone();
+		// 	self
+		// }
+
+		// pub fn get_node(&self, node_id: &str) -> Option<&Node> {
+		// 	self.nodes.iter().find(|node| node.id == node_id)
+		// }
+	}
+}
