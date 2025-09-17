98aba0b11c85a6c2b8854a58c91884344d405a8c
Jimmy Chu <898091+jimmychu0807@users.noreply.github.com>
2025-09-17 18:06:21 +0800

    feat: completed problem-34 DOT-DSL

--- rust/dot-dsl/src/graph_items/edge.rs
+++ rust/dot-dsl/src/graph_items/edge.rs
@@ -1,2 +1,26 @@
-#[derive(Debug, Default, Clone)]
-pub struct Edge {}
+use std::collections::HashMap;
+
+#[derive(Debug, Default, Clone, PartialEq)]
+pub struct Edge {
+	from: String,
+	to: String,
+	attrs: HashMap<String, String>,
+}
+
+impl Edge {
+	pub fn new(from: &str, to: &str) -> Self {
+		Self { from: from.to_string(), to: to.to_string(), attrs: HashMap::new() }
+	}
+
+	pub fn with_attrs(mut self, attrs: &[(&str, &str)]) -> Self {
+		for (key, val) in attrs {
+			self.attrs.insert(key.to_string(), val.to_string());
+		}
+
+		self
+	}
+
+	pub fn attr(&self, key: &str) -> Option<&str> {
+		self.attrs.get(key).map(|v| v.as_str())
+	}
+}
--- rust/dot-dsl/src/lib.rs
+++ rust/dot-dsl/src/lib.rs
@@ -3,6 +3,7 @@ pub mod graph_items;
 pub mod graph {
 	pub use crate::graph_items;
 	pub use graph_items::{edge::Edge, node::Node};
+	use std::collections::HashMap;
 
 	#[derive(Debug, Default, Clone)]
 	pub struct Attr {}
@@ -11,7 +12,7 @@ pub mod graph {
 	pub struct Graph {
 		pub edges: Vec<Edge>,
 		pub nodes: Vec<Node>,
-		pub attrs: Vec<Attr>,
+		pub attrs: HashMap<String, String>,
 	}
 
 	impl Graph {
@@ -19,18 +20,26 @@ pub mod graph {
 			Self::default()
 		}
 
-		// pub fn with_nodes(mut self, nodes: &[Node]) -> Self {
-		// 	self.nodes = nodes.into();
-		// 	self
-		// }
+		pub fn with_nodes(mut self, nodes: &[Node]) -> Self {
+			self.nodes = nodes.to_vec();
+			self
+		}
 
-		// pub fn with_edges(mut self, edges: &Vec<Edge>) -> Self {
-		// 	self.edges = (*edges).clone();
-		// 	self
-		// }
+		pub fn with_edges(mut self, edges: &[Edge]) -> Self {
+			self.edges = edges.to_vec();
+			self
+		}
 
-		// pub fn get_node(&self, node_id: &str) -> Option<&Node> {
-		// 	self.nodes.iter().find(|node| node.id == node_id)
-		// }
+		pub fn with_attrs(mut self, attrs: &[(&str, &str)]) -> Self {
+			for (key, val) in attrs {
+				self.attrs.insert(key.to_string(), val.to_string());
+			}
+
+			self
+		}
+
+		pub fn node(&self, name: &str) -> Option<&Node> {
+			self.nodes.iter().find(|node| name == node.name)
+		}
 	}
 }
