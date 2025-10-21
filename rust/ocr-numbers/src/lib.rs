d8ea41743693e9b4de0b163fd54e8bcbd894123c
Jimmy Chu <898091+jimmychu0807@users.noreply.github.com>
2025-10-21 11:43:53 +0800

    added ocr-number

---
+++ rust/ocr-numbers/src/lib.rs
@@ -0,0 +1,12 @@
+// The code below is a stub. Just enough to satisfy the compiler.
+// In order to pass the tests you can add-to or change any of this code.
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum Error {
+	InvalidRowCount(usize),
+	InvalidColumnCount(usize),
+}
+
+pub fn convert(input: &str) -> Result<String, Error> {
+	todo!("Convert the input '{input}' to a string");
+}
