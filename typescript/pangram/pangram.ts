901691b6d3ea220a3aac7383e0f5e1bfcedaad53
Jimmy Chu <898091+jimmychu0807@users.noreply.github.com>
2026-01-20 12:39:50 +0800

    Remove yarn files and add pangram solution (#67)
    * remove yarn files and add pangram solution
    
    * added path restriction on github CI

---
+++ typescript/pangram/pangram.ts
@@ -0,0 +1,16 @@
+export function isPangram(text: string) {
+  if(text === ''){
+    return false;
+  }
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz'; // 根据意思需要让输入的句子包含所有字母，所以先定一个字母表。
+  const testText = text.toLowerCase(); // 统一用小写来进行比对
+
+  for (let i = 0; i < alphabet.length; i++) { 
+    const char = alphabet[i]; // 从字母表里抽取每一个字母
+    if (!testText.includes(char)) { //看看该字母是否被测试文本包含
+      return false;
+    }
+  }
+  return true;  
+
+}
